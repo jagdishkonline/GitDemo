12d0666c0c323a06800f6df1ab68f6e20524f52c
admin <jagdishkonline@gmail.com>
2020-11-09 02:48:47 +0530

    GitStuff Commited

---
+++ DemoProject/src/HelloworldN.java
@@ -0,0 +1,12 @@
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+			System.out.println("Hello Jagdish");
+				System.out.println("Hello Ganesh");
+
+	}
+
+}
